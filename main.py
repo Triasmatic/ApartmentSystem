e87548cfd36f9c17537acf20d28f254604b7aa12
Aryan Sanyal <Triasmatic@gmail.com>
2022-06-15 11:09:04 -0700

    classes for application and landing page made

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+# reference links
+# https://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+
 import tkinter as tk
 
 
+class RentApp:
+    def __int__(self):
+        # basic window setup
+        window = tk.Tk()
+        button1 = tk.Button()
+        window.title("Tenant Management System")
+        window.geometry('700x550')
 
-window = tk.Tk();
-button1 = tk.Button();
-window.title("Tenant Management System");
-window.geometry('700x550');
+        windowFrames = []
 
 
-window.mainloop();
+        window.mainloop()
+
+class landingPage:
+    def __int__(self):
+        print("landing page window")
+
+
+
+if __name__ == '__main__':
+    RentApp().__int__()
