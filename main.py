1f7f6f0ddd6b8774b2a7d37b31f1be2dc2813386
Aryan Sanyal <Triasmatic@gmail.com>
2022-06-15 10:39:05 -0700

    basic window with title added

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import tkinter as tk
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+window = tk.Tk();
+button1 = tk.Button();
+window.title("Tenant Management System");
+window.geometry('700x550');
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+window.mainloop();
