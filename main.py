ad80fc0b878f4ce634f5cd9f25abf27a259f5513
Ralfbrse <raphaeloburce@gmail.com>
2022-06-21 09:47:29 -0700

    testing

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 # reference links
 # https://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
 
-import tkinter as tk
+from tkinter import *
 
 
 class RentApp:
     def __int__(self):
         # basic window setup
-        window = tk.Tk()
-        tenantBtn = tk.Button(window, text="Tenant Management", bg='orange')
-        rentBtn = tk.Button(window, text="Rent Management")
-        reportBtn = tk.Button(window, text="Finanacial")
-        tenantBtn.grid(column=0, row=0)
-        rentBtn.grid(column=1, row=0)
-        reportBtn.grid(column=2, row=0)
-        #tenantBtn.pack()
-        #rentBtn.pack()
-        #reportBtn.pack()
+        def leave():
+            nope = Button(window, text="don't press", command=exit)
+            nope.pack()
+        window = Tk()
+        tenantBtn = Button(window, text="Tenant Management", bg='orange', command=leave)
+        rentBtn = Button(window, text="Rent Management")
+        reportBtn = Button(window, text="Finanacial")
+        # tenantBtn.grid(column=0, row=0)
+        # rentBtn.grid(column=1, row=0)
+        # reportBtn.grid(column=2, row=0)
+        tenantBtn.pack()
+        rentBtn.pack()
+        reportBtn.pack()
         window.title("Tenant Management System")
         window.geometry('700x550')
 
