08ef4851b5de2ea4163ec12fa662c7394ff73dce
Ralfbrse <raphaeloburce@gmail.com>
2022-06-21 12:45:04 -0700

    cleaning

--- client.py
+++ client.py
@@ -9,13 +9,6 @@ class Client:
         self.expenseList = []
         pass
     
-    def display_main_menu(self) -> None:
-        print("i - input data\nd - display report\nq - quit program\n")
-        return 0
-    
-    def display_input_menu(self) -> None:
-        print("t - add tenant\nr - record rent\ne - record expense\n")
-        return
     
     def input_tenant(self) -> None:
         name = input("Enter tenant name: ")
@@ -29,25 +22,42 @@ class Client:
         
         return
     
-    def display_tenants(self):
+     
+    
+class UI:
+    def main_menu() -> None:
+        return "i - input data\nd - display report\nq - quit program\n"
+    
+    def input_menu() -> None:
+        return "t - add tenant\nr - record rent\ne - record expense\n"
+    
+    def display_tenants(prog):
         print("Room - Name")
         print("-----------")
-        for element in self.tenantList:
-            print(self.tenantList.get(element))
+        for element in prog.tenantList:
+            print(prog.tenantList.get(element))
             
-        return  
-    
-    
+        return 
    
         
 driver = Client()
 
 while True:
-    driver.display_input_menu()
-    option = input()
+    option = input(UI.main_menu())
     
-    if option == "t":
-        driver.input_tenant()
+    if option == "i": #input data 
+        option = input(UI.input_menu())
+        if option == "t":
+            driver.input_tenant()
+        elif option == "r":
+            pass #record rent
+        elif option == "e":
+            pass #record expense
+     
+    elif option == "d": #display report
+        UI.display_tenants(driver)
         
-    elif option == "d":
-        driver.display_tenants()
+    elif option == "q": #quit program
+        break
+    
+print("Logging out...")
